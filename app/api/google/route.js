8c119789fd805c72d8ee77595f74909a5ebdbfad
martinsharma17 <martinsharma18@gmail.com>
2024-06-12 14:18:59 +0545

    logo

--- app/api/google/route.js
+++ app/api/google/route.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
+
 export async function POST(request) {
   const reqBody = await request.json();
   const secret_key = process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY;
